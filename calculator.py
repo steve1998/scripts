51ba1423aba57e90c9ff53f8641ed700dc317c98
Ferdin Handoko <ferdinhandoko@outlook.com>
2018-08-04 22:40:48 +1200

    remove redundant code

--- calculator.py
+++ calculator.py
@@ -2,16 +2,16 @@
 # May be extended to support other features as well as other libraries
 
 def add(x, y):
-    return float(x) + float(y)
+    return x + y
 
 def min(x, y):
-    return float(x) - float(y)
+    return x - y
 
 def multiply(x, y):
-    return float(x) * float(y)
+    return x * y
 
 def divide(x, y):
-    return float(x) / float(y)
+    return x / y
 
 def main():
     check = True
